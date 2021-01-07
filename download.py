222b92a02b5fc5d911e7522ff66376c3fc1f18e3
Sophie Luna Schumann <me@luna.vg>
2021-01-07 14:28:37 +0100

    autopep8

--- download.py
+++ download.py
@@ -10,8 +10,10 @@ args = argparse.ArgumentParser("matrix-download-and-decrypt")
 args.add_argument('-m', '--mxc', nargs=1, required=True, help='mxc:// URL')
 args.add_argument('-k', '--key', nargs=1, required=True, help="k value")
 args.add_argument('-i', '--iv', nargs=1, required=True, help='iv value')
-args.add_argument('-s', '--homeserver', nargs=1, required=False, default=["https://matrix-client.matrix.org"],
-                  help='Matrix homeserver to contact')
+args.add_argument(
+    '-s', '--homeserver', nargs=1, required=False,
+    default=["https://matrix-client.matrix.org"],
+    help='Matrix homeserver to contact')
 args.add_argument('-o', '--output', nargs=1, required=True, help='output file')
 
 
@@ -20,9 +22,10 @@ def b64decode(s: str) -> bytes:
     s_bin += b'=' * (4 - len(s_bin) % 4)
     return urlsafe_b64decode(s_bin)
 
+
 def download_mxc(mxcurl: str, homeserver: str) -> bytes:
     if 'mxc://' in mxcurl:
-        mxcurl = mxcurl[mxcurl.index('mxc://')+6:]
+        mxcurl = mxcurl[mxcurl.index('mxc://') + 6:]
 
     r = requests.get(
         homeserver + MEDIA_API + f"{mxcurl}"
@@ -30,12 +33,15 @@ def download_mxc(mxcurl: str, homeserver: str) -> bytes:
     r.raise_for_status()
     return r.content
 
+
 def decrypt_data(data: bytes, key: str, iv: str) -> bytes:
     decoded_key = b64decode(key)
     decoded_iv = b64decode(iv)
-    c = AES.new(decoded_key, AES.MODE_CTR, nonce=decoded_iv[:8], initial_value=decoded_iv[8:])
+    c = AES.new(decoded_key, AES.MODE_CTR,
+                nonce=decoded_iv[:8], initial_value=decoded_iv[8:])
     decrypted = c.decrypt(data)
     return decrypted
+
 
 if __name__ == '__main__':
     argv = args.parse_args()
@@ -43,4 +49,3 @@ if __name__ == '__main__':
     decrypted_data = decrypt_data(data, argv.key[0], argv.iv[0])
     with open(argv.output[0], 'wb') as f:
         f.write(decrypted_data)
-
